33a11e9ddee43572c5481604836d52ccb4439233
halac123b <halac123b@gmail.com>
2024-10-29 04:22:29 +0700

    Create BrowserWindow

--- hello-world/main.js
+++ hello-world/main.js
@@ -1,1 +1,17 @@
 console.log("Hello");
+
+const electron = require("electron");
+const app = electron.app;   // App instance from electron module
+
+const path = require("path");
+const url = require("url");
+
+let win;
+
+function createWindow() {
+    // Class của window cho phép present nội dung từ trang web như trên browser
+    win = new electron.BrowserWindow();
+    win.loadURL(url.format({
+        pathname: path.join()
+    }));
+}
