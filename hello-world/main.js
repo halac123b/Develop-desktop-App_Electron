db56483c9e7b5d2689e7b51265e4b4c71e6d0c1c
halac123b <halac123b@gmail.com>
2024-11-04 01:34:09 +0700

    Load URL for BrowserWindow

--- hello-world/main.js
+++ hello-world/main.js
@@ -11,7 +11,15 @@ let win;
 function createWindow() {
     // Class của window cho phép present nội dung từ trang web như trên browser
     win = new electron.BrowserWindow();
+    // Load url nội dung cần đc window hiển thị
     win.loadURL(url.format({
-        pathname: path.join()
+        // Protocol sử dụng để access url
+        /// Ở đây ta dùng file ở local nên protocol file://
+        /// Nếu là url trên mạng thì dùng http://
+        protocol: "file",
+        // Dùng file local nên cần khai báo path đến file
+        pathname: path.join(__dirname, "index.html"),
+        // Thêm // vào sau tên protocol (file://)
+        slashes: true
     }));
 }
